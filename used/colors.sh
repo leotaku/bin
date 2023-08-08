3acd56d97098e2e51c0cdc1b8390d36c96db7162
Leo Gaskin <leo.gaskin@le0.gs>
2023-08-08 21:49:21 +0200

    Commit all generally useful scripts
    These are all my scripts that could theoretically be useful to pretty
    much anyone running a resonably modern Linux system. Scripts that
    require more specific setup will be commited at a later date.

---
+++ used/colors.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+for x in $(seq 0 8); do
+    for i in $(seq 30 37); do
+        for a in $(seq 40 47); do
+            echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "
+        done
+        echo
+    done
+done
