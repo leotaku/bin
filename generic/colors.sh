43170a11f399a8589df53512ffeed2bf0a8f8245
Leo Gaskin <leo.gaskin@le0.gs>
2024-03-01 21:38:00 +0100

    Fix various bashisms in shell scripts
    $ checkbashisms generic/* specific/* sunset/*

--- generic/colors.sh
+++ generic/colors.sh
@@ -2,7 +2,7 @@
 for x in $(seq 0 8); do
     for i in $(seq 30 37); do
         for a in $(seq 40 47); do
-            echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "
+            printf "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "
         done
         echo
     done
